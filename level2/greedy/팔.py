f83c042c41c1dce01ed000356b89101e97ca5902
123qpq <45002168+123qpq@users.noreply.github.com>
2021-03-01 19:57:25 +0900

    Create 팔.py

---
+++ level2/greedy/팔.py
@@ -0,0 +1,14 @@
+#1105
+import sys
+l, r = map(str, sys.stdin.readline().split())
+if len(l) != len(r):
+    print(0)
+    sys.exit(0)
+cnt = 0
+for i in range(len(l)):
+    if l[i] == r[i]:
+        if l[i] == '8':
+            cnt += 1
+    else:
+        break
+print(cnt)
