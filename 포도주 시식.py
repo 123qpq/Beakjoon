85b5dea1793af3a79498fc64ebfd605b3dd47b12
123qpq <45002168+123qpq@users.noreply.github.com>
2021-03-05 08:48:10 +0900

    Update 포도주 시식.py
    more better dp

--- 포도주 시식.py
+++ 포도주 시식.py
@@ -1,9 +1,8 @@
+#2156
 n = int(input())
-wines = [0] + [int(input()) for _ in range(n)]
-dp = [0, wines[1]] # 앞에꺼를 마셨을 때 / 앞앞꺼를 마셨을 때
-if n > 1:
-    dp.append(wines[1]+wines[2])
-for i in range(3, n+1):
-    dp.append(max(dp[i-1], dp[i-2] + wines[i], dp[i-3] + wines[i-1] + wines[i]))
+wines = [int(input()) for _ in range(n)]
+a, b, c = 0, 0, 0
+for i in wines:
+    a, b, c = max(a, b, c), a+i, b+i
 
-print(dp[-1])
+print(max(a, b, c))
