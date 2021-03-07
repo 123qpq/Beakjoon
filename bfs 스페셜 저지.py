5835df6aa9a3882e9570584ea671171eb3622226
123qpq <45002168+123qpq@users.noreply.github.com>
2021-03-07 19:08:04 +0900

    Create bfs 스페셜 저지.py

---
+++ bfs 스페셜 저지.py
@@ -0,0 +1,36 @@
+#16940
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+lst = [[] for i in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    lst[a].append(b)
+    lst[b].append(a)
+answer = list(map(int, sys.stdin.readline().split()))
+
+q = deque([1])
+visited = [1]
+rank = [-1 for i in range(n+1)]
+res = []
+
+for i in range(1, n+1):
+    rank[answer[i-1]] = i
+
+for i in range(1, n+1):
+    lst[i] = sorted(lst[i], key=lambda x : rank[x])
+
+while q:
+    node = q.popleft()
+    res.append(node)
+
+    for n in lst[node]:
+        if n not in visited:
+            visited.append(n)
+            q.append(n)
+
+if res == answer:
+    print(1)
+else:
+    print(0)
