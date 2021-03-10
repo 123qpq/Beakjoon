dcc350c282548564dd025e301bb0bfe26206a02c
123qpq <45002168+123qpq@users.noreply.github.com>
2021-03-10 14:51:45 +0900

    Update 색종이 만들기.py

--- silver/level3/색종이 만들기.py
+++ silver/level3/색종이 만들기.py
@@ -1,17 +1,28 @@
-def search(half, table):
-    global blue, white
-    if half == 0:
-        print()
-    else:
-        for i in range(4):
-            for j in range(half):
-                for k in range(half-1):
-                    if table[j][k] != table[j][k+1]:
-                        
-    
+#1167
+import sys
+def check(x, y, length):
+    pv = table[y][x]
+    for i in range(y,y+length):
+        for j in range(x, x+length):
+            if table[i][j] != pv:
+                return False
+    return True
 
-n = int(input())
-table = [int(input()) for _ in range(n)]
-blue = 0
-white = 0
-search(n//2, table)
+def find(x, y, length):
+    if check(x, y, length):
+        res[table[y][x]] += 1
+    else:
+        length //= 2
+        find(x, y, length)
+        find(x, y+length, length)
+        find(x+length, y, length)
+        find(x+length, y+length, length)
+        return
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    table = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+    res = [0, 0]
+    find(0, 0, n)
+    for i in res:
+        print(i)
