65b5018b830729ec91198db02262228a1e92058f
123qpq <45002168+123qpq@users.noreply.github.com>
2021-03-22 23:14:08 +0900

    Create 웜홀.py

---
+++ gold/level4/웜홀.py
@@ -0,0 +1,34 @@
+#1865
+import sys
+tc = int(sys.stdin.readline())
+
+def solution():
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            for node, dist in graph[j]:
+                if distlst[node] > dist + distlst[j]:
+                    distlst[node] = dist + distlst[j]
+                    if i == n:
+                        return True
+    return False
+
+for _ in range(tc):
+    n, m, w = map(int, sys.stdin.readline().split())
+    graph = {x : [] for x in range(1, n+1)}
+
+    for _ in range(m):
+        s, e, t = map(int, sys.stdin.readline().split())
+        graph[s].append((e, t))
+        graph[e].append((s, t))
+
+    for _ in range(w):
+        s, e, t = map(int, sys.stdin.readline().split())
+        graph[s].append((e, -t))
+        
+    distlst = [float('inf')]*(n+1)
+    distlst[e] = 0
+
+    if solution():
+        print("YES")
+    else:
+        print("NO")
