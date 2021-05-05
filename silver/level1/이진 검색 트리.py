bc82e4dc89cd021ee5d0d856b18c7c765111430b
123qpq <45002168+123qpq@users.noreply.github.com>
2021-05-05 22:48:32 +0900

    Create 이진 검색 트리.py

---
+++ silver/level1/이진 검색 트리.py
@@ -0,0 +1,23 @@
+#5639
+import sys
+import bisect
+sys.setrecursionlimit(100000000)
+
+def sol(start, end):
+    if start > end:
+        return
+    
+    right = bisect.bisect_left(tree, tree[start], start+1, end+1)
+    sol(start+1, right-1) #왼쪽
+    sol(right, end) #오른쪽
+    print(tree[start]) #루트출력
+
+tree = []
+while count <= 10000:
+    try:
+        temp = int(input())
+    except:
+        break
+    tree.append(temp)
+    count += 1
+sol(0, len(tree)-1)
